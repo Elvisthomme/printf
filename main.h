e6d6060d8076ed4bc54a88357683fc7bbaf05026
FNTettey <98092552+FNTettey@users.noreply.github.com>
2022-04-19 01:10:27 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,8 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
+/**
+ *struct print_func - struct to select specific print function
+ *@print_ft: format specifier
+ *@p_func: pinter to function
+ */
+typedef struct print_func
+{
+	char print_ft;
+	int (*p_func)(va_list l);
+} pr_f;
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *);
