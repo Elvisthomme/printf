f678c8b0ca13d6dfd516d7061cb59f977abb9cea
elvisthomme <elvisthomme@gmail.com>
2022-04-19 15:24:14 +0100

    Fix betty style

--- print_in_base.c
+++ print_in_base.c
@@ -3,15 +3,13 @@
 /**
  * print_in_base - prints an integer a specifier base
  * @n: the integer to print
- * @b: the base
+ * @c: the base
  * Return: the number of printed digit
  */
 int print_in_base(unsigned long int n, char c)
 {
-	unsigned long int positive_value, b;
-	long int number_of_digit = 1, divider, i, j, p, count = 0;
-	char x16[] = {'a', 'b', 'c', 'd', 'e', 'f'};
-	char X16[] = {'A', 'B', 'C', 'D', 'E', 'F'};
+	unsigned long int positive_value = n, b;
+	long int number_of_digit = 1, divider, i, j, count = 0;
 
 	switch (c)
 	{
@@ -28,7 +26,6 @@ int print_in_base(unsigned long int n, char c)
 		case 'x':
 			b = 16;
 	}
-	positive_value = n;/* Count the number of digit */
 	while (positive_value > b - 1)
 	{
 		number_of_digit++;
@@ -46,22 +43,40 @@ int print_in_base(unsigned long int n, char c)
 		if (b < 11)/*Print the first digit*/
 			count += _putchar('0' + n / divider);
 		else
-			if ((n / divider) > 10)
-				switch (c)
-				{
-					case 'x':
-						p = (n / divider) % 10;
-						count += _putchar(x16[p]);
-						break;
-					case 'X':
-						p = (n / divider) % 10;
-						count += _putchar(X16[p]);
-						break;
-				}
-			else
-				count += _putchar('0' + n / divider);
+			count += print_num_up_to_10(n, divider, c);
 		/* get the part of the number that we have not printed*/
 		n -= (n / divider) * divider;
 	}
+	return (count);
+}
+
+/**
+  * print_num_up_to_10 - print in a base greather than 10
+  * @n: the integer to print
+  * @d: the divider
+  * @c: the base
+  * Return: the number of printed digit
+  */
+int print_num_up_to_10(int n, int d, char c)
+{
+	int count = 0, p;
+	char x16[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+	char X16[] = {'A', 'B', 'C', 'D', 'E', 'F'};
+
+	if ((n / d) > 10)
+	{
+		p = (n / d) % 10;
+		switch (c)
+		{
+			case 'x':
+				count += _putchar(x16[p]);
+				break;
+			case 'X':
+				count += _putchar(X16[p]);
+				break;
+		}
+	}
+	else
+		count += _putchar('0' + n / d);
 	return (count);
 }
